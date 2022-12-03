55106565fe6850717f9c8bea1566a3b92705be14
benvarian <88700858+benvarian@users.noreply.github.com>
2022-12-03 11:56:36 +0800

    completed modulo test operation

--- tests_operations.py
+++ tests_operations.py
@@ -63,4 +63,6 @@ def test_modulo():
     WHEN:  passed in to the function modulo
     THEN:  the resulting is the modulo of the two number
     """
-    pass
+    assert modulo(8,2) == 0
+    assert modulo(9, 2) == 1
+
