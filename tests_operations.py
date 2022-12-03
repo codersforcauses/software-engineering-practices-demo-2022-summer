c36adf82cbcb53ca39c35b3ab827a376136a4ec7
HUIXIN-TW <huixin.yang.tw@gmail.com>
2022-12-03 11:57:25 +0800

    create test for division operation#4

--- tests_operations.py
+++ tests_operations.py
@@ -1,6 +1,5 @@
 # from operations import addition, subtraction, multiplication, division, exponentiation, modulo
 
-
 def test_addition():
     """
     GIVEN: Two numbers
@@ -64,3 +63,12 @@ def test_modulo():
     THEN:  the resulting is the modulo of the two number
     """
     pass
+
+def test_division():
+    """
+    GIVEN: two numbers
+    WHEN: passed in to the function division
+    THEN: the resulting is the division of the two numbers
+    """
+    assert division(8, 4) == 2
+    assert division(3, 3) == 1
